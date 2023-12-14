44005548beb4bab666d405a6dc72a427d8d8cafe
LovelyToaster <lovelytoaster94@outlook.com>
2023-12-14 15:30:02 +0800

    refactor: 数据库地址修改

--- src/SQL_Connection.java
+++ src/SQL_Connection.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class SQL_Connection {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/student_optional_course_management?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    static final String DB_URL = "jdbc:mysql://113.31.125.148:3306/student_optional_course_management?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     static final String user = "root";
     static final String password = "zyn20030527";
 
